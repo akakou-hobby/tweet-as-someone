04b2bd9850edc7466075f87829351d036aa8a3f3
akakou <akakou571@gmail.com>
2020-04-01 17:28:58 +0900

    postをうけとったよ

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sys
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return render_template('index.html')
+
+@app.route("/post", methods=['POST'])
+def post():
+    content = request.form['content']
+    password = request.form['password']
+
+    print(content, password, file=sys.stderr)
+
     return render_template('index.html')
 
 if __name__ == "__main__":
